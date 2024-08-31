913ebb5ae73227e79fd805fa6ebaeba77a8c03fc
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-08-31 18:53:07 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -61,3 +61,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
