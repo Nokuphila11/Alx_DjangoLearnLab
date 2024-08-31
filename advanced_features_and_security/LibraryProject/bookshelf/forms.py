503c34671951c52f147d269ad817341833fe374c
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-08-31 19:11:50 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']  # Include fields you want to expose
+        widgets = {
+            'published_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
