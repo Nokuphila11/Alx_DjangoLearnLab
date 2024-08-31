457b3f87cbfa5c80669d941cf14302436cf307e3
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-08-31 19:14:10 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -54,4 +54,16 @@ def search_books(request):
     else:
         books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+from django.shortcuts import render, redirect
+from .forms import BookForm
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Redirect to a page showing the list of books
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
