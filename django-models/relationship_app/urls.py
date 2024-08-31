26c3de7fc4067973a8ad061f40e004fa989065da
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-08-31 17:37:01 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -32,3 +32,12 @@ urlpatterns = [
     path('register/', register, name='register'),  # Ensure 'register' view is linked here
 ]
 
+# relationship_app/urls.py
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+]
