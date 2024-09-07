657eb8fed2daa6ef42988357feac3708becd1fde
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-07 08:22:36 +0200

    Update views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+# ListView to retrieve all books
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# DetailView to retrieve a single book by ID
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
