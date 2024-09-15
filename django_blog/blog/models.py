3134164689b278d9faec6a27419255d0043322e6
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-15 19:29:27 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+# models.py
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
