f10c9cced4c94915fb365fc9b54fa4671a2ae575
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-15 19:50:43 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -155,3 +155,23 @@ def search_posts(request):
         posts = Post.objects.all()
     
     return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
+# views.py (in your blog app)
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', id=post_id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
