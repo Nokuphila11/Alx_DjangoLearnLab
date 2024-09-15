2cbbaee528b4c25eb8529072dc308e40fc56ac05
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-15 19:31:21 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -30,3 +30,12 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email',)
+
+# forms.py
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include 'tags'
