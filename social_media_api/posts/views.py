d05b95d51c92cf9cf7452740b164e0c8d6bcec56
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-20 00:13:00 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -46,3 +46,21 @@ class UserFeedView(generics.ListAPIView):
         # Get posts only from users the current user follows
         followed_users = self.request.user.following.all()
         return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .models import Post
+from .serializers import PostSerializer
+
+User = get_user_model()
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
