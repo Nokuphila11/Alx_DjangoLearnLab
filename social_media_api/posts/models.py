21f0b5d3963bf41f7b45bfea24366bb7bb1fb95f
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-09-20 00:16:04 +0200

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -25,3 +25,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+
+from django.db import models
+from django.contrib.auth import get_user_model
+from posts.models import Post
+
+User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')
